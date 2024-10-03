bbb14d2c5192d54b5116ac1ccfa7e22088ced1e2
13k <k@13k.dev>
2024-10-03 12:35:48 -0300

    feat(mo2-rootbuilder): update scripts

---
+++ scripts/mo2-rootbuilder/post_uninstall.ps1
@@ -0,0 +1,16 @@
+$app = $args[0]
+$appDir = appdir "$app"
+
+if ($null -eq $appDir) {
+    exit
+}
+
+if (-not (Test-Path "$appDir")) {
+    exit
+}
+
+$pluginDir = "$appDir\\current\\plugins\\rootbuilder"
+
+if (Test-Path "$pluginDir") {
+  Remove-Item -Force "$pluginDir"
+}
