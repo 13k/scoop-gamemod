c81c6d9cfb55c1c33fc3d50a7bf29d74122fffee
13k <k@13k.dev>
2024-10-03 12:46:17 -0300

    fix(mo2-rootbuilder): fix checkver script

--- scripts/mo2-rootbuilder/checkver.ps1
+++ scripts/mo2-rootbuilder/checkver.ps1
@@ -4,15 +4,10 @@ $url = "https://api.github.com/repos/Kezyma/ModOrganizer-Plugins/releases/tags/r
 $resp = Invoke-WebRequest -Uri "$url" -ErrorAction Stop
 $obj = ConvertFrom-Json $resp.Content -ErrorAction Stop
 
-$asset = $obj.assets
-    | Where-Object { $_.name -match 'rootbuilder.([\d.]+).zip' }
-    | ForEach-Object {
-        @{
-            version = $matches[1]
-            url = $_.browser_download_url
-        }
-    }
-    | Select-Object -Last 1
+$asset = $obj.assets |
+    Where-Object { $_.name -match 'rootbuilder.([\d.]+).zip' } |
+    ForEach-Object { @{ version = $matches[1]; url = $_.browser_download_url; } } |
+    Select-Object -Last 1
 
 if ($null -eq $asset) {
     throw "Could not find a suitable asset from ${url}"
